3570802f0bd19569652a159f6692a26fd5be8c7a
Marckenley Dorsainvil <marckenlygbeau@gmail.com>
2025-06-14 16:52:56 +0400

    fix ts error

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,13 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: "https",
-            hostname: "randomuser.me",
-          },
-        ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "randomuser.me",
+        port: "",
+        pathname: "/api/portraits/**", // This allows all paths under /api/portraits/
       },
+    ],
+  },
 };
 
 export default nextConfig;
